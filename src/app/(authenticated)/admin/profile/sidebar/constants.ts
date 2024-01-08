5fb4b105694ac4e823b96614ba0143c9c9ba9a5e
Nguyen Trong Ninh <nguyentrongninh24072003@gmail.com>
2024-01-08 13:43:42 +0700

    ok

--- src/app/(authenticated)/admin/profile/sidebar/constants.ts
+++ src/app/(authenticated)/admin/profile/sidebar/constants.ts
@@ -4,106 +4,12 @@ export const mainNavItems = [
     icon: 'news',
     items: [
       {
-        title: 'Products',
-        href: '/products',
+        title: 'Trang chủ',
+        href: '/',
         description: 'All the products we have to offer.',
 
         items: [],
       },
     ],
   },
-  // {
-  //   title: 'Events',
-  //   icon: 'events',
-  //   items: [
-  //     {
-  //       title: 'View Events',
-  //       href: '/events',
-  //       description: 'All the products we have to offer.',
-
-  //       items: [],
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'App Service',
-  //   icon: 'appService',
-  //   items: [
-  //     {
-  //       title: 'View App Service',
-  //       href: '/app-service',
-  //       description: 'All the products we have to offer.',
-
-  //       items: [],
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Settings',
-  //   icon: 'settings',
-  //   items: [
-  //     {
-  //       title: 'About Us',
-  //       href: '/about-us',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //     {
-  //       title: 'Term and Conditions',
-  //       href: '/conditions',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //     {
-  //       title: 'Referral Policy',
-  //       href: '/referral-policy',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //     {
-  //       title: 'Withdraw Policy',
-  //       href: '/withdraw-policy',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //     {
-  //       title: 'Deposit Policy',
-  //       href: '/deposit-policy',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //     {
-  //       title: 'Banners',
-  //       href: '/banners',
-  //       description: 'All the products we have to offer.',
-  //       items: [],
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Wallet',
-  //   icon: 'wallet',
-  //   items: [
-  //     {
-  //       title: 'View App Service',
-  //       href: '/wallet',
-  //       description: 'All the products we have to offer.',
-
-  //       items: [],
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Benefits',
-  //   icon: 'benefits',
-  //   items: [
-  //     {
-  //       title: 'View Benefits',
-  //       href: '/benefits',
-  //       description: 'All the products we have to offer.',
-
-  //       items: [],
-  //     },
-  //   ],
-  // },
 ];
